9f6efa5819159762fd54ffe635376a66f2e040c2
Matthew D. Ball <m.d.ball2@ncl.ac.uk>
2016-11-30 16:25:52 +0000

    Commented the Encryption class

--- partone/src/main/java/uk/co/m4numbers/csc8102/partone/Encryption.java
+++ partone/src/main/java/uk/co/m4numbers/csc8102/partone/Encryption.java
@@ -26,14 +26,25 @@ import java.security.*;
 /**
  * Class Name - Encryption
  * Package - uk.co.m4numbers.csc8102.partone
- * Desc of Class -
+ * Desc of Class - A class centred around the encryption of a file for the
+ *  user. All the functions within this class allow for the fulfillment of
+ *  that requirement.
  * Author(s) - M. D. Ball
  * Last Mod: 29/11/2016
  */
 class Encryption {
 
+    /**
+     * Encrypt a file with a given password, deleting the original file in
+     * the process
+     *
+     * @param filename The file that we're going to encrypt
+     * @param password The password we're going to encrypt it with
+     * @throws Exception If something goes wrong within individual methods
+     */
     void encrypt(String filename, String password) throws Exception
     {
+        //Read in the plaintext and get the two halves of our keys for later
         String plaintext = Utils.read_file(filename);
         DerivedKeys key_set = derive_keys(password.getBytes("utf-8"));
 
@@ -42,21 +53,43 @@ class Encryption {
                 plaintext.getBytes("utf-8"));
         byte[] hmac = generate_hmac(iv, ciphertext, key_set.mac_code);
 
+        //Join all of our parts together to create our final output byte array
+        // that we pass to be written to file
         byte[] joined = Utils.concatenate_byte_arrays(
                 Utils.concatenate_byte_arrays(iv, ciphertext), hmac);
 
         write_to_file(joined, filename);
     }
 
+    /**
+     * Derive the two keys from the provided password that we're going to be
+     * using for our cipher and hash functions respectively.
+     *
+     * @param password The password that we hash in order to get our keys and
+     *                 codes
+     * @return A collection object of a 16-byte AES key and a 16-byte MAC code
+     * @throws NoSuchAlgorithmException If SHA-256 doesn't exist or cannot be
+     *  used
+     */
     private DerivedKeys derive_keys(byte[] password) throws NoSuchAlgorithmException
     {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         md.update(password);
         byte[] digest = md.digest();
-        return new DerivedKeys(Utils.split_byte_array(digest, 0, 16),
-                Utils.split_byte_array(digest, 16, 32));
+        return new DerivedKeys(
+                //AES key
+                Utils.split_byte_array(digest, 0, 16),
+                //MAC code
+                Utils.split_byte_array(digest, 16, 32)
+        );
     }
 
+    /**
+     * Generate a securely random sequence of 16 bytes using Java's
+     * SecureRandom class to act as an initial vector for our CBC bit later on
+     *
+     * @return 16 bytes of secure pseudo-randomness
+     */
     private byte[] generate_initial_vector()
     {
         SecureRandom sr = new SecureRandom();
@@ -65,6 +98,18 @@ class Encryption {
         return ret_bytes;
     }
 
+    /**
+     * Generate a 20-byte message signature according to all the input data
+     * that we used to generate this message
+     *
+     * @param iv The initial vector (hashed with the ciphertext)
+     * @param ciphertext The ciphertext itself (hashed with the iv)
+     * @param mac The mac code that we use as a secret key for signing the
+     *            message
+     * @return A 20-byte HMAC output to be attached to the encrypted file
+     * @throws NoSuchAlgorithmException If HmacSHA1 doesn't exist
+     * @throws InvalidKeyException If HmacSHA1 can't be used on the key
+     */
     private byte[] generate_hmac(byte[] iv, byte[] ciphertext, byte[] mac)
             throws NoSuchAlgorithmException, InvalidKeyException
     {
@@ -76,11 +121,13 @@ class Encryption {
     }
 
     /**
+     * Generate the ciphertext from the given data via the cipher as given in
+     * the coursework specification (of AES in CBC mode using PKC5 padding).
      *
      * @param iv 16-byte random data
      * @param key 16-byte AES key
      * @param plaintext plaintext bytes to encrypt
-     * @return Ciphertext
+     * @return The ciphertext according to our inputs
      */
     private byte[] generate_ciphertext(byte[] iv, byte[] key, byte[] plaintext)
             throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
@@ -93,10 +140,22 @@ class Encryption {
         return aes_cipher.doFinal(plaintext);
     }
 
+    /**
+     * Write the encrypted data into a file that we can then decrypt at a
+     * later date if we see fit to. We put the contents in [file_name].8102
+     * and delete the original file from the records when we are done.
+     *
+     * @param file_contents What we are going to be writing to the file
+     * @param file_name The name of the file that we're going to write to...
+     *                  sort of
+     * @throws IOException If file deletion is hard and scary
+     */
     private void write_to_file(byte[] file_contents, String file_name) throws IOException
     {
         File encrypted_file = new File(file_name + ".8102");
         FileOutputStream fos = new FileOutputStream(encrypted_file);
+        //Encode the contents of the file into something which is a mite
+        // harder to corrupt
         fos.write(new sun.misc.BASE64Encoder().encode(file_contents).getBytes("utf-8"));
         fos.flush();
         fos.close();
